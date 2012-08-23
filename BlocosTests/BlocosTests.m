3e8de9caba90c12305be0ca3db59dc1c051d598a
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-23 09:21:02 -0300

    Initial Commit

---
+++ BlocosTests/BlocosTests.m
@@ -0,0 +1,32 @@
+//
+//  BlocosTests.m
+//  BlocosTests
+//
+//  Created by Rafael Brigagão Paulino on 23/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "BlocosTests.h"
+
+@implementation BlocosTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BlocosTests");
+}
+
+@end
